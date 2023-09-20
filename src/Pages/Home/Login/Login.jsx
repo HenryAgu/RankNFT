9d3b33e8cc3907fb08bcd638febe599a48a4edc1
Henry Agu <aguhenrychuks@gmail.com>
2023-09-20 01:03:19 +0100

    feat: login page updated

--- src/Pages/Home/Login/Login.jsx
+++ src/Pages/Home/Login/Login.jsx
@@ -4,15 +4,15 @@ import "./style/Login.scss";
 const Login = () => {
   return (
     <div className='login'>
-        <h1>Welcome To RankNFT</h1>
+        <h1>Welcome To <span>RankNFT</span></h1>
         <form action="">
             <h3>Log In</h3>
             <div className="inner_form">
-                <label htmlFor="">Username</label>
+                <label htmlFor="">Username:</label>
                 <input type="text" placeholder='Enter Username'/>
             </div>
             <div className="inner_form">
-                <label htmlFor="">Password</label>
+                <label htmlFor="">Password:</label>
                 <input type="password" placeholder='Enter Password'/>
             </div>
             <button>Submit</button>
